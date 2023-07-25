e7095e067e8c9ca0dacb225f103e58ec3e4235ac
King Nodens <judith.zamora.go@gmail.com>
2023-07-25 16:32:08 -0600

    correcciones 25.07.23

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -4,13 +4,19 @@ import PropTypes from 'prop-types';
 const ContactForm = ({ addContact }) => {
   const [name, setName] = useState('');
   const [number, setNumber] = useState('');
+  const [state, setState] = useState({name: '', number: ''});
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    addContact(name);
-    setName('');
-    setNumber('');
+    addContact({state});
+    setState({name: '', number: ''});
+    
   };
+
+  const handleChange = e => {
+  setState ((prevState) => ({...prevState, [e.target.name]: e.target.value}));
+  };
+  console.log(state);
 
   return (
     <form onSubmit={handleSubmit}>
@@ -19,7 +25,7 @@ const ContactForm = ({ addContact }) => {
         type="text"
         name="name"
         value={name}
-        onChange={(e) => setName(e.target.value)}
+        onChange={handleChange}
         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
         title="Name"
         required
@@ -29,16 +35,16 @@ const ContactForm = ({ addContact }) => {
         type="tel"
         name="number"
         value={number}
-        onChange={(e) => setNumber(e.target.value)}
+        onChange={handleChange}
         pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
         title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
         required
       />
       <button type="submit">Add Contact</button>
-      
     </form>
   );
 };
+
 ContactForm.propTypes = {
   addContact: PropTypes.func.isRequired,
 };
